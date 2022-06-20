448c0a8a617cca367bade807f496c94e3850f29f
hsmak <husain.alkhamis@gmail.com>
2022-06-20 08:32:03 -0500

    committing changes..

--- docker/zeppelin/_run-zeppelin.sh
+++ docker/zeppelin/_run-zeppelin.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-export SPARK_HOME=/opt/tools/spark/spark-3.2.1-bin-hadoop3.2
-docker run -p 8080:8080  -v $SPARK_HOME:/opt/spark -e SPARK_HOME=/opt/spark --rm --name zeppelin apache/zeppelin:0.10.1
+#Link: https://zeppelin.apache.org/docs/latest/setup/operation/configuration.html
+export SPARK_HOME=/opt/tools/spark/spark-3.3.0-bin-hadoop3
+docker  run -p 8080:8080 \
+        -v $SPARK_HOME:/opt/spark \
+        -e SPARK_HOME=/opt/spark \
+        -e ZEPPELIN_JAVA_OPTS="-Dzeppelin.spark.enableSupportedVersionCheck=false" \
+        --rm --name zeppelin apache/zeppelin:0.10.1
